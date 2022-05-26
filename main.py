30f4f52b93323dcab25613098dcb89376dd748a6
blessingarizona <41776649+blessingarizona@users.noreply.github.com>
2022-05-26 17:16:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+
+def is_anagram(s1, s2):
+    print(sorted(s1), sorted(s2))
+    if(sorted(s1) == sorted(s2)):
+        return True
+    return False
+
+print(is_anagram("hello", "check"))
+
+def is_anagram(s1, s2):
+    print(sorted(s1), sorted(s2))
+    if(sorted(s1) == sorted(s2)):
+        return True
+    return False
+
+print(is_anagram("below", "elbow"))
+
